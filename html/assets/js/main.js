dea65055dc487f7c7475bd865592d26d4b6fc24d
Levy Chacon <levychacon@yahoo.com>
2021-08-20 12:33:12 -0300

    javascript adicionado

---
+++ html/assets/js/main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+
+for(const elements of toggle){
+    elements.addEventListener('click', function(){
+        nav.classList.toggle('show')
+    })
+}
+
+const links= document.querySelectorAll('nav ul li a');
+
+for(const link in links){
+    link.addEventListener('click', function(){
+        nav.classList.remove('show')
+    })
+}
